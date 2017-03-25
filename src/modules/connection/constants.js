5b2156db40918d647b9423560f8802b4eae574e1
Francisco Preller <francisco.preller@gmail.com>
2017-03-25 12:38:47 +1100

    Added constant to connection module

--- src/modules/connection/constants.js
+++ src/modules/connection/constants.js
@@ -5,5 +5,5 @@ export const IDENTIFY_SESSION = `OPEN_CONNECTION`;
 export const CLOSE_SESSION = 'CLOSE_CONNECTION';
 export const REMOVE_SESSION = 'REMOVE_CONNECTION';
 
-// Websocket actions
-export const OPEN_CONNECTION = `${WS_PREFIX}/open.connection`;
+// Web-socket actions
+export const OPEN_CONNECTION = `${WS_PREFIX}/connection.open`;
