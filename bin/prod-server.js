a3bfa52c39ebbe69f459df0f5130cd7085d974cd
Francisco Preller <francisco.preller@gmail.com>
2017-04-12 20:56:15 +1000

    Dockerize container

---
+++ bin/prod-server.js
@@ -0,0 +1,9 @@
+// Lock in production environment
+process.env.NODE_ENV = 'production';
+process.env.PORT = 80;
+
+const project = require('../config/project.config');
+const server = require('../server/main');
+
+server.listen(project.server_port);
+console.log(`Server is now running on port ${project.server_port}.`);
