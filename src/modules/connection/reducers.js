269102c0decfe1705194b1a6e8c77945318cb93d
Francisco Preller <francisco.preller@gmail.com>
2017-04-12 20:55:54 +1000

    Add connectivity state

--- src/modules/connection/reducers.js
+++ src/modules/connection/reducers.js
@@ -1,13 +1,34 @@
-import { SESSION_CLOSED, SESSION_INIT } from '../session/constants';
+import { REHYDRATE } from 'redux-persist/constants';
+import { SESSION_CLOSED, SESSION_INIT, SESSION_CONNECTED } from '../session/constants';
 
 // Initial State
 const initialState = [];
 
 // Handlers
 const ACTION_HANDLERS = {
+  [REHYDRATE]: (state, action) => {
+    const { connections } = action.payload;
+    if (connections) {
+      return connections.map(conn => Object.assign({}, conn, {
+        connected: false,
+        connecting: false,
+      }));
+    }
+    return state;
+  },
   [SESSION_INIT]: (state, action) => [
     ...state,
-    Object.assign({}, action.payload.connection, { uuid: action.payload.uuid }),
+    Object.assign({}, action.payload.connection, {
+      uuid: action.payload.uuid,
+      connected: false,
+      connecting: true,
+    }),
+  ],
+  [SESSION_CONNECTED]: (state, action) => [
+    ...state.map((conn) => (conn.uuid !== action.payload.uuid) ? conn : Object.assign({}, conn, {
+      connected: true,
+      connecting: false,
+    })),
   ],
   [SESSION_CLOSED]: (state, action) => [
     ...state.filter((connection) => connection.uuid !== action.payload.uuid),
--- src/modules/session/utils.js
+++ src/modules/session/utils.js
@@ -1,33 +1,3 @@
-export function parseLines(lines) {
-  return lines.map((line) => (
-    line.map(fragment => parseLineFragment(fragment)).join(''))
-  );
-}
-
-export function parseLineFragment(fragment) {
-  let styles = `color: ${fragment.codes.fg}; background: ${fragment.codes.bg};`;
-  if (fragment.codes.types) {
-    fragment.codes.types.forEach((t) => {
-      switch (t) {
-        case 'bold':
-          // For now do nothing, later change color
-          // styles += ' font-weight: bold;';
-          break;
-        case 'italic':
-          styles += ' text-decoration: italic;';
-          break;
-        case 'underline':
-          styles += ' text-decoration: underline;';
-          break;
-        default:
-          break;
-      }
-    });
-  }
-  // Replace all whitespaces with non breaking spaces then inject it into span
-  return `<span style="${styles}">${escapeForHtml(fragment.text)}</span>`;
-}
-
 export function escapeForHtml(str) {
   return String(str)
     .replace(/&/g, '&amp;')
